ef020e3765f75d1e87c2aec01ece437e1a46c84b
Trang Luong <luongtrang014@gmail.com>
2022-06-13 15:51:53 +0800

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const IndexPage = ({ data }) => {
           name="viewport"
           content="width=device-width, initial-scale=1"
         ></meta>
-        <title>My Title</title>
+        <title>Kinka Finance</title>
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet"
